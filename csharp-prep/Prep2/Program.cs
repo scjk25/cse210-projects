aaf892b8f1322889ec8f1f4abf7bc6d805350362
Scott Kishpaugh <scottkishpaugh@Scotts-MacBook-Air-2.local>
2023-06-01 23:28:20 -0600

    Completed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        
+        int percent = int.Parse(percentage);
+
+        string letter = "";
+
+        if (percent >=90)
+        {
+            letter = "A";
+        }
+        else if (percent >=80)
+        {
+            letter = "B";
+        }
+        else if (percent >=70)
+        {
+            letter = "C";
+        }
+        else if (percent >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >=70)
+        {
+            Console.WriteLine("Congrats you passed the class!!!!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you failed the course");
+        }
+
     }
 }
